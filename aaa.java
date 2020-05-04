bba0406bf47a0ce81bad4fce03dbe67eb2c3cc87
残梦 <54528046+994963634@users.noreply.github.com>
2020-05-04 10:59:17 +0800

    Add files via upload

---
+++ aaa.java
@@ -0,0 +1,17 @@
+public class aaa {
+
+    public static void main(String[] args) {
+
+       byte b1 = 3;
+
+       byte b2 = 4;
+
+       int b3 = b1 + b2;
+
+       System.out.println("b3 = " + b3);
+	for(int i=0; i<args.length; i++)
+	System.out.println(args[i]);
+
+    } 
+
+}
